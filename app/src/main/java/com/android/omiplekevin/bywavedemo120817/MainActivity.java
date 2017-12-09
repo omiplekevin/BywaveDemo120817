f1d46a97710814dacc8f4f9a1d60d2278e0e6280
omiplekevin <omiple.kevin@gmail.com>
2017-12-09 09:27:20 +0800

    base project files

---
+++ app/src/main/java/com/android/omiplekevin/bywavedemo120817/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.omiplekevin.bywavedemo120817;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
